02142565e0db161a361cffc081a0612d9de93054
SOME1HING <yashprakash2005@gmail.com>
2023-05-28 21:01:07 +0530

    Revert "Update app.js"
    This reverts commit d0132e39efb383949bbeaf049ad75ec1131480d0.

--- src/app.js
+++ src/app.js
@@ -1,44 +1,19 @@
 const express = require("express");
 const { json } = require("express");
+const { ErrorResponseObject } = require("./common/http");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-let userTokens;
-getAllTokens().then((tokens) => (userTokens = new Set(tokens)));
-
-function refreshToken() {
-  getAllTokens().then((tokens) => (userTokens = new Set(tokens)));
-}
-setInterval(refreshToken, 10000);
-
-function validateToken(req, res, next) {
-  const { token } = req.query;
-
-  if (!token) {
-    return res.status(404).send("Token not found.");
-  }
-
-  const isValid = userTokens.has(token);
-
-  if (!isValid) {
-    return res.status(402).send("Invalid token.");
-  }
-
-  next();
-}
-
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(json());
-app.use("/api", require("./routes/api"));
-app.use("/ask", validateToken, require("./routes/ask"));
-app.use("/image", validateToken, require("./routes/image"));
-app.use("/news", validateToken, require("./routes/news"));
-app.use("/search", validateToken, require("./routes/search"));
-app.use("/videos", validateToken, require("./routes/videos"));
-app.use("/wiki", validateToken, require("./routes/wikipedia"));
-app.use("/chat", validateToken, require("./routes/chat"));
+app.use("/", routes);
+
+app.all("*", (req, res) =>
+  res.status(404).json(new ErrorResponseObject("route not defined"))
+);
 
 app.listen(PORT, () =>
   console.log(`server running in http://localhost:${PORT}`)
