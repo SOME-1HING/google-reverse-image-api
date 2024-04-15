dae0ed123977a1238d4a22424636276f46000358
SOME1HING <yashprakash2005@gmail.com>
2024-04-15 11:36:41 +0530

    Added Vercel Web Analytics

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { json } = require("express");
 const { ErrorResponseObject } = require("./common/http");
 const routes = require("./routes");
+const { inject } = require("@vercel/analytics");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+inject();
 app.use(json());
 app.use("/", routes);
 
